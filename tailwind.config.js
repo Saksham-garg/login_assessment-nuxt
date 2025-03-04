0878bead9b4a3cb8f10c5cbaf459e62552665f2a
Saksham-garg <sakshamgarg142@hotmail.com>
2025-03-04 12:54:28 +0530

    fix: tailwind config and auto imports

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,15 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./components/**/*.{js,vue,ts}",
-    "./layouts/**/*.vue",
-    "./pages/**/*.vue",
-    "./plugins/**/*.{js,ts}",
+    "./components/**/*.{vue,js,ts}",
+    "./layouts/**/*.{vue,js,ts}",
+    "./pages/**/*.{vue,js,ts}",
     "./app.vue",
+    "./plugins/**/*.{js,ts}",
     "./error.vue",
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
-
+};
